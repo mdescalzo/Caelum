d1c526a130819f5f937ffa1a7e10d98fdab2c4c0
Mark Descalzo <mdescalzo@mac.com>
2025-07-11 10:50:23 -0600

    Initial Xcode project commit

---
+++ Caelum/CaelumApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaelumApp.swift
+//  Caelum
+//
+//  Created by Mark Descalzo on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CaelumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaelumTests/CaelumTests.swift
@@ -0,0 +1,17 @@
+//
+//  CaelumTests.swift
+//  CaelumTests
+//
+//  Created by Mark Descalzo on 7/11/25.
+//
+
+import Testing
+@testable import Caelum
+
+struct CaelumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
